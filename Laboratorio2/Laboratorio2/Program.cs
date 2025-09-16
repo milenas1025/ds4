4d1db69388afeefbad224d6324d2541b12653409
milenas1025 <milenasimmons1025@gmail.com>
2025-09-15 20:35:50 -0500

    Agregar Laboratorio2

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Laboratorio2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //Ejemplo utilizando las variables de instancia de Clase.
+            client.FirstName = "Milena";
+            client.LastName = "Simmons";
+            client.Age = 21;
+            client.Id = 1;
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+    public class  Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
