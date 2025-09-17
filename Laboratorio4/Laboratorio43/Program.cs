837953c8447108027cb3c1e061c259ca0c8f1ad9
milenas1025 <milenasimmons1025@gmail.com>
2025-09-16 21:17:32 -0500

    Laboratorio4

---
+++ Laboratorio4/Laboratorio43/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int suma, cant, valor, promedio;
+        string linea;
+        suma = 0;
+        cant = 0;
+        do
+        {
+            Console.Write("Ingrese un valor entero (0 para terminar): ");
+            linea = Console.ReadLine();
+            valor = int.Parse(linea);
+            if (valor != 0)
+            {
+                suma = suma + valor;
+                cant = cant + 1;
+            }
+        } while (valor != 0);
+        if (cant != 0)
+        {
+            promedio = suma / cant;
+            Console.Write("El promedio de los valores ingresados es: ");
+            Console.Write(promedio);
+        }
+        else
+        {
+            Console.Write("No se ingresaron valores.");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Laboratorio4/Laboratorio44/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la nota del estudinate");
+        float score = float.Parse(Console.ReadLine());
+
+        if (score >= 70)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Su nota es {score} ha aprobado");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Su nota es {score} ha reporbado, debe repetir");
+        }
+    }
+}
