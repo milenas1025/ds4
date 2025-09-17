a5d34782653d40029443a8e0bb1e8a3d4c2bd65c
milenas1025 <milenasimmons1025@gmail.com>
2025-09-16 19:17:41 -0500

    carga de laboratorio 2

---
+++ Laboratorio2/Laboratorio21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Laboratorio21
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //Asignando valor a variable estatica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+
+        //Declarando Variable estatica.
+        public static int Valor;
+    }
+}
