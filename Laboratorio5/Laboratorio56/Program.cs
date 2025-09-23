0cc9443223243991c0ed5b5fd846f91d8fcce8fe
milenas1025 <milenasimmons1025@gmail.com>
2025-09-22 19:42:44 -0500

    Subir lab5

---
+++ Laboratorio5/Laboratorio56/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+{
+    {"Francia", "París" },
+    {"España", "Madrid"},
+    {"Italia", "Roma"}
+};
+foreach (KeyValuePair<string, string> par in paisesYCapitales)
+{
+       Console.WriteLine("La capital de " + par.Key + " es " + par.Value + ".");
+}
---
+++ Laboratorio5/Labratorio53/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+string[] frutas = { "manzana", "plátano", "naranja" };
+foreach (string fruta in frutas)
+{
+    Console.WriteLine(fruta);
+}
