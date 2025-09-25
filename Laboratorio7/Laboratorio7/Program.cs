bf5941e328dc61d962675ded0c689297d2c85dfe
milenas1025 <milenasimmons1025@gmail.com>
2025-09-24 19:09:22 -0500

    lab7

---
+++ Laboratorio7/Laboratorio7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Laboratorio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratorio7/Laboratorio72/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laboratorio72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JuegoDeDados j = new JuegoDeDados();
+            j.Jugar();
+        }   
+    }
+}
